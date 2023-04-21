8c2481983f9d3f7eb4d16aa6755f113055f842f2
mg-sweet <thetpainghtu@gmail.com>
2023-04-21 21:40:25 +0630

    adding flight1

--- class.py
+++ class.py
@@ -5,4 +5,18 @@ class Flights:
         self.destination = destination
         self.duration = duration
 
-        
+
+def main():
+
+    #create Flight
+    flight1 = Flights(origin='New York', destination='Paris', duration=540)
+
+    flight1.duration = 550
+
+    print(flight1.origin)
+    print(flight1.destination)
+    print(flight1.duration)
+
+
+if __name__ == "__main__":
+    main()
